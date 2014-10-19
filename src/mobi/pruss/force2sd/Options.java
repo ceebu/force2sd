03bd4028ee8d205db8981f4fa2c7ba4b55ece83f
arpruss <arpruss@gmail.com>
2014-10-19 00:51:03 +0000

    1.26

--- src/mobi/pruss/force2sd/Options.java
+++ src/mobi/pruss/force2sd/Options.java
@@ -1,6 +1,8 @@
 package mobi.pruss.force2sd;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.content.pm.ActivityInfo;
@@ -9,10 +11,12 @@ import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.text.Html;
 
 public class Options extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 	public static final String PREF_CHECK_ORPHANS = "checkOrphans";
 	public static final String PREF_METHOD = "method";
+	public static final String PREF_BULK = "bulk";
 	public static final int METHOD_OLD = 0;
 	public static final int METHOD_NEW = 1;
 	
@@ -34,6 +38,23 @@ public class Options extends PreferenceActivity implements OnSharedPreferenceCha
 		super.onCreate(icicle);
 
 		addPreferencesFromResource(R.xml.options);
+		
+		Preference lic = (Preference) findPreference("license");
+		lic.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+		             public boolean onPreferenceClick(Preference preference) {
+		                 AlertDialog alertDialog = new AlertDialog.Builder(Options.this).create();
+		                 alertDialog.setTitle("Licenses and copyrights");
+		                 alertDialog.setMessage(Html.fromHtml(Utils.getAssetFile(Options.this, "licenses.txt")));
+		                 alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "OK", 
+		                 	new DialogInterface.OnClickListener() {
+		                     public void onClick(DialogInterface dialog, int which) {finish();} });
+		                 alertDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+		                     public void onCancel(DialogInterface dialog) {finish();} });
+		                 alertDialog.show();
+						return true;
+		             }
+		         });
+
 	}
 
 	@Override
@@ -92,4 +113,6 @@ public class Options extends PreferenceActivity implements OnSharedPreferenceCha
 		String s = options.getString(Options.PREF_METHOD, "1");
 		return Integer.parseInt(s);
 	}
+
+	
 }
